5e01f0572ccdd486614ee061bfc30ecab5b4d76d
Suraj <info.blogtheorem@gmail.com>
2021-12-30 20:22:32 +0530

    thumbnail:many array display

--- public/script/thumbnail.js
+++ public/script/thumbnail.js
@@ -6,15 +6,50 @@ let width = document.querySelector(".width");
 let thumbnail_img = document.querySelector(".thumbnail_img");
 let x = document.querySelector(".x");
 let heading = document.querySelector(".heading");
+let thumbnail_box = document.querySelector(".thumbnail_box");
 
 // GET THUMBNAIL ARRAY
 function getThumbnail(data) {
-  x.style.visibility = "visible";
   //   btn_download.style.visibility = "visible";
-  height.innerText = data.thumbnails[3].height;
-  width.innerText = data.thumbnails[3].width;
-  thumbnail_img.src = data.thumbnails[3].url;
+  //   height.innerText = data.thumbnails[3].height;
+  //   width.innerText = data.thumbnails[3].width;
+  //   thumbnail_img.src = data.thumbnails[3].url;
+  // heading.innerText = data.title;
+
   heading.innerText = data.title;
+
+  data.thumbnails.forEach((element) => {
+    console.log(element.url);
+    // let size = document.createElement("div");
+    // size.classList.add("size");
+    // let height = document.createElement("span");
+    // height.classList.add("height");
+    // height.innerText = element.height;
+    // let x = document.createElement("span");
+    // x.classList.add("x");
+    // x.style.visibility = "visible";
+    // let width = document.createElement("span");
+    // width.classList.add("width");
+    // width.innerText = element.width;
+    // size.appendChild(height);
+    // size.appendChild(x);
+    // size.appendChild(width);
+    // let thumbnail_container = document.createElement("div");
+    // thumbnail_container.classList.add("thumbnail_container");
+    // let p = document.createElement("p");
+    // p.innerText = "sdfsdfs";
+    let thumbnail_container = document.createElement("div");
+    thumbnail_container.classList.add("thumbnail_container");
+
+    let thumbnail_img = document.createElement("img");
+    thumbnail_img.classList.add("thumbnail_img");
+    thumbnail_img.src = element.url;
+    thumbnail_container.append(thumbnail_img);
+    thumbnail_box.append(thumbnail_container);
+
+    // // thumbnail_img.appendChild(thumbnail_container);
+    // thumbnail_img.append(thumbnail_box);
+  });
 }
 
 // VALIDATE URL
