772ec696a48e420493c829eb4c346760ae950cfe
Suraj <info.blogtheorem@gmail.com>
2022-01-03 21:18:02 +0530

    add:server details

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ytdl = require("ytdl-core");
 const cors = require("cors");
 let path = require("path");
+const { log } = require("console");
 
 const app = express();
 
@@ -56,41 +57,52 @@ app.get("/tag", (req, res) => {
 
 // YOUTUBE TO MP3 CONVERTER
 app.get("/audio", async (req, res) => {
-  const videoID = req.query.youtubeURL;
-  convertURL(videoID);
-  let info = await ytdl.getInfo(videoID);
-  let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+  try {
+    const videoID = req.query.youtubeURL;
+    convertURL(videoID);
+    let info = await ytdl.getInfo(videoID);
+    let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
 
-  res.send(
-    JSON.stringify({
-      audioURL: audioFormats[1].url,
-      thumbnail: info.videoDetails.thumbnails,
-      title: info.videoDetails.title,
-    })
-  );
+    res.send(
+      JSON.stringify({
+        audioURL: audioFormats[1].url,
+        thumbnail: info.videoDetails.thumbnails,
+        title: info.videoDetails.title,
+      })
+    );
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 // THUMBNAIL DOWNALOAD
 app.get("/thumbnaildata", async (req, res) => {
-  const videoID = req.query.youtubeURL;
-  convertURL(videoID);
-  let info = await ytdl.getInfo(videoID);
-  res.send(JSON.stringify(info.videoDetails));
+  try {
+    const videoID = req.query.youtubeURL;
+    convertURL(videoID);
+    let info = await ytdl.getInfo(videoID);
+    res.send(JSON.stringify(info.videoDetails));
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 // TAG DOWNLOAD
 app.get("/tagdata", async (req, res) => {
-  const videoID = req.query.youtubeURL;
-  convertURL(videoID);
-  let info = await ytdl.getInfo(videoID);
-
-  res.send(
-    JSON.stringify({
-      keywords: info.videoDetails.keywords,
-      thumbnails: info.videoDetails.thumbnails,
-      title: info.videoDetails.title,
-    })
-  );
+  try {
+    const videoID = req.query.youtubeURL;
+    convertURL(videoID);
+    let info = await ytdl.getInfo(videoID);
+    res.send(
+      JSON.stringify({
+        keywords: info.videoDetails.keywords,
+        thumbnails: info.videoDetails.thumbnails,
+        title: info.videoDetails.title,
+      })
+    );
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.listen(port, () => {
